715260e0a15e7d79327ea562ad828350d9195d4e
MDevoldere <mdevoldere@users.noreply.github.com>
2024-01-10 14:16:50 +0100

    App Console

---
+++ ConsoleApps/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+static List<int> TrouverLesdiviseurs(int nombre)
+{
+
+    List<int> resultat = new();
+
+    for (int i = 2; i < nombre; i++)
+    {
+        if (nombre % i == 0)
+        {
+            resultat.Add(i);
+        }
+    }
+
+    return resultat;
+}
+
+static List<int> TrouverDiviseursRecursif(int nombre, int i, List<int> resultat)
+{
+    if (nombre % i == 0)
+    {
+        resultat.Add(i);
+    }
+
+    if (i < nombre)
+    {
+        TrouverDiviseursRecursif(nombre, ++i, resultat);
+    }
+
+    return resultat;
+}
