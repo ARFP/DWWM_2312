8ca87131e2f8b4acd21868052498c921e672e99b
MDevoldere <mdevoldere@users.noreply.github.com>
2024-07-12 10:48:14 +0200

    intro organisation code

---
+++ web-frontend/03-js/08-organisation-code/main.js
@@ -0,0 +1,25 @@
+import { DbCar } from "./DbCar.js";
+
+let dbCar = new DbCar();
+let cars = await dbCar.fetchAll();
+
+const result = document.getElementById('result');
+console.log(result);
+
+
+
+function genererEmail(personne) { //personne = "Mike DEV";
+    
+    let tableau = personne.split(' ');
+    let prenom = tableau[0];
+    let nom = tableau[1];
+
+    return nom + prenom + '@gmail.com';
+}
+
+
+function genererTableauHTML() {
+    let td = document.createElement('td');
+    td.textContent = genererEmail('DEV', 'Mike')
+
+}
