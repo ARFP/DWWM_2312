08114df771031022926acb1fffafdf0a524930fb
MDevoldere <mdevoldere@users.noreply.github.com>
2024-08-22 16:20:41 +0200

    Create ApiCountries

---
+++ web-backend-asp/ApiCountries/ApiCountries/Models/Continent.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiCountries.Models
+{
+    [Table("continent")]
+    public class Continent
+    {
+        [Key]
+        [Column("continent_id")]
+        public int Id { get; set; }
+
+        [MaxLength(20, ErrorMessage = "Un continent doit contenir 20 caractères maximum")]
+        [MinLength(2)]
+        [Column("continent_name")]
+        public string ContinentName { get; set; }
+
+        public int ContinentArea { get; set; }
+
+    }
+}
