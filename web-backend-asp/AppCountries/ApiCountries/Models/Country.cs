6fd85906401c8ea9814f39454d7bf454dfd5a069
MDevoldere206 <10086604+mdevoldere@users.noreply.github.com>
2025-02-17 13:13:25 +0100

    debut api country

---
+++ web-backend-asp/AppCountries/ApiCountries/Models/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiCountries.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string CountryCode { get; set; }
+
+        [Required]
+        public string CountryName { get; set; }
+
+    }
+}
